e641a1aa547a7c3df043c715f3108de6ac0fdebe
Matt Wehner <mattwehnernz@gmail.com>
2023-02-12 19:42:28 -0700

    Started Sample

---
+++ Assets/Samples/BasicMarkerReceivers/ManualMarkerReceiver.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using BCIEssentials.Utilities;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BCIEssentials.LSL.Samples
+{
+    public class ManualMarkerReceiver : MonoBehaviour
+    {
+        [Header("Configuration")]
+        [SerializeField] private LSLServiceProvider _provider;
+        [SerializeField] private string _streamName = "MyStream";
+        [SerializeField] private bool _getOnlyLatest;
+        
+        [Header("UI")]
+        [SerializeField] private Button _requestButton;
+        [SerializeField] private Text _responseText;
+
+        [Space(20), InspectorReadOnly]
+        public LSLMarkerReceiver LslMarkerReceiver;
+
+        private void Start()
+        {
+            _requestButton.onClick.AddListener(GetResponses);
+            _provider.TryGetMarkerReceiverByStreamName(_streamName, out LslMarkerReceiver);
+        }
+
+        public void GetResponses()
+        {
+            var responses = _getOnlyLatest ? LslMarkerReceiver.GetLatestResponses() : LslMarkerReceiver.GetResponses();
+            
+            var responseStrings = responses.Select(r => r.Value[0]).ToArray();
+            _responseText.text = $"Responses:\n{string.Join(",\n", responseStrings)}";
+        }
+    }
+}
