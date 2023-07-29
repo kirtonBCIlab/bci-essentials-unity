09d74f3d2231e764a6b722b6e3116bafeff93615
Matt Wehner <mattwehnernz@gmail.com>
2023-07-29 14:25:26 -0600

    Update ResolveStreamTests.cs

--- Assets/Tests/Runtime/LSL/ResolveStreamTests.cs
+++ Assets/Tests/Runtime/LSL/ResolveStreamTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using BCIEssentials.LSLFramework;
 using BCIEssentials.Tests.Utilities;
 using LSL;
 using LSL4Unity.Utils;
@@ -9,53 +10,85 @@ using UnityEngine.TestTools;
 
 namespace BCIEssentials.Tests.LSLService
 {
+    
     public class ResolveStreamTests : PlayModeTestRunnerBase
     {
-        private const float k_testTimeoutDuration = 10;
-        private const float k_streamResolveTimeout = 1;
-
-        [Test]
-        public void WhenStreamOutletCreatedAndResolveStreams_ThenStreamAvailableImmediately()
-        {
-            var streamName = Guid.NewGuid().ToString();
-            NewStreamOutlet(streamName);
-
-            var resolvedStreams = LSL.LSL.resolve_streams(k_streamResolveTimeout);
-
-            foreach (var streamInfo in resolvedStreams)
-            {
-                if (streamInfo.name() != streamName) continue;
-                
-                Assert.Pass("A matching stream was found");
-                return;
-            }
-            
-            Assert.Fail("No matching stream was found.");
-        }
+        private const float k_streamResolveTimeout = 0.1f;
+        private static readonly float[] k_limitValues = { 0, 0.1f, 0.5f, 1, 2, 5, 10 };
 
         [UnityTest]
-        public IEnumerator WhenStreamOutletCreatedAndUseResolver_ThenStreamsResolvedWithinReasonableTime()
+        public IEnumerator WhenStreamOutletCreatedAndResolveStreams_ThenStreamsResolvedWithinTimeLimit([ValueSource(nameof(k_limitValues))] float limit)
         {
+            var streamFound = false;
             var streamName = Guid.NewGuid().ToString();
-            bool streamResolved = false;
-            
-            //Resolver won't find a stream unless it's had samples pushed.
-            NewStreamOutlet(streamName).push_sample(new[]{"marker"}); 
-            AddComponent<Resolver>().OnStreamFound += info =>
-            {
-                streamResolved = info.name() == streamName;
-            };
-
-            yield return new WaitForSecondsRealtime(1); //Continue test next frame for resolver coroutine to run again.
+            NewStreamOutlet(streamName);
             
             var duration = 0f;
-            while (!streamResolved && duration < k_testTimeoutDuration)
+            while (!streamFound && duration <= limit)
             {
+                var resolvedStreams = LSL.LSL.resolve_streams(k_streamResolveTimeout);
+
+                foreach (var streamInfo in resolvedStreams)
+                {
+                    if (streamInfo.name() == streamName)
+                    {
+                        streamFound = true;
+                        break;
+                    }
+                }
+                
                 duration += Time.fixedDeltaTime;
                 yield return null;
             }
             
-            Assert.True(streamResolved);
+            Assert.True(streamFound);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator WhenStreamOutletCreatedAndUseResolver_ThenStreamsResolvedWithinTimeLimit([ValueSource(nameof(k_limitValues))]float limit)
+        {
+            var streamFound = false;
+            var streamName = Guid.NewGuid().ToString();
+            
+            NewStreamOutlet(streamName).push_sample(new[]{"marker"}); //Resolver won't find a stream unless it's had samples pushed.
+            AddComponent<Resolver>().OnStreamFound += info =>
+            {
+                if (info.name() != streamName) return;
+                streamFound = true;
+            };
+
+            var duration = 0f;
+            while (!streamFound && duration <= limit)
+            {
+                duration += Time.fixedDeltaTime;
+                yield return null;
+            }
+
+            Assert.True(streamFound);
+        }
+
+        [UnityTest]
+        public IEnumerator WhenStreamOutletCreatedAndUseLSLServiceProvider_ThenStreamsResolvedWithinTimeLimit([ValueSource(nameof(k_limitValues))]float limit)
+        {
+            var streamFound = false;
+            var streamName = Guid.NewGuid().ToString();
+            NewStreamOutlet(streamName);
+            var provider = AddComponent<LSLServiceProvider>();
+            
+            var duration = 0f;
+            while (!streamFound && duration <= limit)
+            {
+                if (provider.GetMarkerReceiverByName(streamName) != null)
+                {
+                    streamFound = true;
+                }
+                
+                duration += Time.fixedDeltaTime;
+                yield return null;
+            }
+
+            Assert.True(streamFound);
         }
 
         private static StreamOutlet NewStreamOutlet(string streamName)
