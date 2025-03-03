59d643af46edd5b0f29fd2641a330dbdd18c70f1
Twig <littleb88888888@gmail.com>
2025-03-03 11:27:01 -0700

    Filled out marker writer tests - added basic formatting tests for each marker write method

--- Tests/Runtime/LSLFramework/LSLMarkerWriterTests.cs
+++ Tests/Runtime/LSLFramework/LSLMarkerWriterTests.cs
@@ -33,7 +33,87 @@ namespace BCIEssentials.Tests.LSLFramework
             int trainingTarget, string expectedSampleValue
         )
         {
-            OutStream.PushMIMarker(objectCount, windowLength, trainingTarget);
+            OutStream.PushMIMarker
+            (
+                objectCount, windowLength, trainingTarget
+            );
+            AssertPulledSample(expectedSampleValue);
+        }
+
+        [Test]
+        [TestCase(2, 1.5f, 1, "switch,2,1,1.50")]
+        public void PushSwitchMarker_WhenMarkerPushed_ThenPulledWithCorrectFormat
+        (
+            int objectCount, float windowLength,
+            int trainingTarget, string expectedSampleValue
+        )
+        {
+            OutStream.PushSwitchMarker
+            (
+                objectCount, windowLength, trainingTarget
+            );
+            AssertPulledSample(expectedSampleValue);
+        }
+
+        [Test]
+        [TestCase(4, 1.5f, new[] {12.5f,18.7f,24.4f,30.1f}, 2, "ssvep,4,2,1.50,12.5,18.7,24.4,30.1")]
+        public void PushSSVEPMarker_WhenMarkerPushed_ThenPulledWithCorrectFormat
+        (
+            int objectCount, float windowLength, float[] frequencies,
+            int trainingTarget, string expectedSampleValue
+        )
+        {
+            OutStream.PushSSVEPMarker
+            (
+                objectCount, windowLength,
+                frequencies, trainingTarget
+            );
+            AssertPulledSample(expectedSampleValue);
+        }
+
+        [Test]
+        [TestCase(6, 1.5f, new[] {15f}, 2, "tvep,6,2,1.50,15")]
+        public void PushTVEPMarker_WhenMarkerPushed_ThenPulledWithCorrectFormat
+        (
+            int objectCount, float windowLength, float[] frequencies,
+            int trainingTarget, string expectedSampleValue
+        )
+        {
+            OutStream.PushTVEPMarker
+            (
+                objectCount, windowLength,
+                frequencies, trainingTarget
+            );
+            AssertPulledSample(expectedSampleValue);
+        }
+
+        [Test]
+        [TestCase(8, 1, 3, "p300,s,8,3,1")]
+        public void PushSingleFlashP300Marker_WhenMarkerPushed_ThenPulledWithCorrectFormat
+        (
+            int objectCount, int activeObject,
+            int trainingTarget, string expectedSampleValue
+        )
+        {
+            OutStream.PushSingleFlashP300Marker
+            (
+                objectCount, activeObject, trainingTarget
+            );
+            AssertPulledSample(expectedSampleValue);
+        }
+
+        [Test]
+        [TestCase(8, new[] {1,3,5,7}, 3, "p300,m,8,3,1,3,5,7")]
+        public void PushMultiFlashP300Marker_WhenMarkerPushed_ThenPulledWithCorrectFormat
+        (
+            int objectCount, int[] activeObjects,
+            int trainingTarget, string expectedSampleValue
+        )
+        {
+            OutStream.PushMultiFlashP300Marker
+            (
+                objectCount, activeObjects, trainingTarget
+            );
             AssertPulledSample(expectedSampleValue);
         }
     }
