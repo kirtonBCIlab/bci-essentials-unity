04dd7400863f841dcdc730c3deba6f1522a4495d
Twig <littleb88888888@gmail.com>
2025-03-19 10:10:34 -0600

    Drafted custom keycode property drawer - sketched a custom property drawer to improve the keybind process, based on [this example](https://discussions.unity.com/t/custom-inspector-to-capture-a-single-key-press/713848/6)

---
+++ Editor/PropertyDrawers/KeyCodeDrawer.cs
@@ -0,0 +1,61 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace BCIEssentials.Editor
+{
+    [CustomPropertyDrawer(typeof(KeyCode))]
+    public class KeyCodeDrawer: PropertyDrawer
+    {
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            property.enumValueIndex = (int)KeyCodeField(position, (KeyCode)property.enumValueIndex);
+        }
+
+
+        public static KeyCode KeyCodeField(Rect position, KeyCode currentValue)
+        {
+            int keyboardControlId = GUIUtility.GetControlID(FocusType.Keyboard);
+            Event currentEvent = Event.current;
+
+            switch(currentEvent.GetTypeForControl(keyboardControlId))
+            {
+                case EventType.Repaint:
+                    GUIStyle style = EditorStyles.miniButton;// GUI.skin.GetStyle("TextField");
+                    string s = currentValue.ToString();
+                    if (GUIUtility.keyboardControl == keyboardControlId)
+                        s = "Press a Key to Bind";
+                    style.Draw(position, new GUIContent(s), keyboardControlId);
+                break;
+                case EventType.MouseDown:
+                    if (
+                        position.Contains(currentEvent.mousePosition) &&
+                        currentEvent.button == 0 && GUIUtility.hotControl == 0
+                    ) {
+                        GUIUtility.hotControl = keyboardControlId;
+                        GUIUtility.keyboardControl = keyboardControlId;
+                        currentEvent.Use();
+                    }
+                break;
+                case EventType.MouseUp:
+                    if (GUIUtility.hotControl == keyboardControlId)
+                    {
+                        GUIUtility.hotControl = 0;
+                        currentEvent.Use();
+                    }
+                break;
+                case EventType.KeyDown:
+                    if (GUIUtility.keyboardControl == keyboardControlId)
+                    {
+                        GUIUtility.hotControl = 0;
+                        GUIUtility.keyboardControl = 0;
+                        currentEvent.Use();
+                        return currentEvent.keyCode;
+                    }
+                break;
+            }
+
+            return currentValue;
+        }
+    }
+}
