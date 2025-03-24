2aa16c1be0a83c9bf73d64b7f47fd53f7e4e784b
Twig <littleb88888888@gmail.com>
2025-03-24 12:27:48 -0600

    Tweaked inspector field order

--- Runtime/Scripts/Behaviors/BCIControllerBehavior.cs
+++ Runtime/Scripts/Behaviors/BCIControllerBehavior.cs
@@ -21,6 +21,15 @@ namespace BCIEssentials.ControllerBehaviors
         /// The type of BCI behavior implemented.
         /// </summary>
         public abstract BCIBehaviorType BehaviorType { get; }
+
+        [SerializeField, Min(-1)]
+        [Tooltip("The applications target frame rate [Hz].\n"
+            + "0 results in no override being applied.\n"
+            + "-1 or higher than 0 is still applied."
+        )]
+        protected int targetFrameRate = 60;
+
+
         [FoldoutGroup("Controller Registration")]
         [SerializeField]
         [Tooltip("Register and Unregister with the BCI Controller instance using Start and OnDestroy")]
@@ -38,10 +47,15 @@ namespace BCIEssentials.ControllerBehaviors
         private BCIControllerInstance _selfRegistrationTarget;
 
 
-
         [FoldoutGroup("Stimulus Presenting Objects")]
         [Tooltip("Engine Tag used to programmatically identify Stimulus Presenting Objects")]
         public string SPOTag = "BCI";
+
+        [SerializeField]
+        [InspectorName("Selectable Objects")]
+        [Tooltip("Provide an initial set of SPO.")]
+        protected List<SPO> _selectableSPOs = new();
+        protected int SPOCount => _selectableSPOs.Count;
 
         [Header("Factory Setup")]
         [SerializeField]
@@ -53,18 +67,6 @@ namespace BCIEssentials.ControllerBehaviors
         [ShowIf("_spoFactory")]
         [Tooltip("Whether to automatically trigger the setup factory when initialized")]
         public bool FactorySetupRequired;
-
-        [SerializeField, Space]
-        [InspectorName("Selectable Objects")]
-        [Tooltip("Provide an initial set of SPO.")]
-        protected List<SPO> _selectableSPOs = new();
-        protected int SPOCount => _selectableSPOs.Count;
-
-        
-        [Header("System Properties and Targets")]
-        [SerializeField, Min(-1)]
-        [Tooltip("The applications target frame rate [Hz]. 0 results in no override being applied. -1 or higher than 0 is still applied.")]
-        protected int targetFrameRate = 60;
 
 
         private int __uniqueID = 1;
