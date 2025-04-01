8e985778667eedbdb189d140fbcf380171507678
Twig <littleb88888888@gmail.com>
2025-04-01 12:20:39 -0600

    Added spo population utilities - split population method enum into scope and method - added monobehaviour extension methods to fetch spo references by type of tag in an enum-specified scope

---
+++ Runtime/Scripts/Models/SpoPopulation.cs
@@ -0,0 +1,17 @@
+namespace BCIEssentials.ControllerBehaviors
+{
+    public enum SPOPopulationScope
+    {
+        Children,
+        ChildrenOfParent,
+        Global,
+    }
+
+    public enum SPOPopulationMethod
+    {
+        Type,
+        Tag,
+        Factory,
+        PreDefined
+    }
+}
