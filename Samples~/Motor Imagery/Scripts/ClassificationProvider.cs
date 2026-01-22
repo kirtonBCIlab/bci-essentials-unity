26f07c5cacefd94484dde04fdd4eec3baf1d3c9c
Twig <littleb88888888@gmail.com>
2026-01-22 11:24:20 -0700

    Drafted simple MI classification provider for sample

---
+++ Samples~/Motor Imagery/Scripts/ClassificationProvider.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using BCIEssentials.Behaviours;
+using BCIEssentials.LSLFramework;
+using BCIEssentials.Selection;
+using UnityEngine;
+
+public class ClassificationProvider : CoroutineBehaviour, IBCIMarkerSource, ISelector
+{
+    public LSLMarkerWriter MarkerWriter { get; set; }
+    public static bool InputValue { get; private set; }
+    public float EpochLength = 0.5f;
+
+
+    public void MakeSelection(int index)
+    {
+        InputValue = index > 0;
+    }
+
+
+    protected override IEnumerator Run()
+    {
+        WaitForSeconds epochDelay = new(EpochLength);
+        while (true)
+        {
+            MarkerWriter.PushMIClassificationMarker(2, EpochLength);
+            yield return epochDelay;
+        }
+    }
+}
