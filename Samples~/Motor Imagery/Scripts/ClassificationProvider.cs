ec6281f1adb3321cdf2d8f95f0802eb1cce992e2
Twig <git@twig.gay>
2026-02-23 11:40:09 -0700

    Updated samples with LSL reworks

--- Samples~/Motor Imagery/Scripts/ClassificationProvider.cs
+++ Samples~/Motor Imagery/Scripts/ClassificationProvider.cs
@@ -5,19 +5,19 @@ using BCIEssentials.LSLFramework;
 using BCIEssentials.Selection;
 using UnityEngine;
 
-public class ClassificationProvider : CoroutineBehaviour, IBCIMarkerSource, ISelector
+public class ClassificationProvider : CoroutineBehaviour, IBCIMarkerSource, IPredictionSink
 {
     public event Action ClassificationStarted;
     public event Action ClassificationEnded;
 
-    public LSLMarkerWriter MarkerWriter { get; set; }
+    public MarkerWriter MarkerWriter { get; set; }
     public bool InputValue { get; private set; }
     public float EpochLength = 0.5f;
 
 
-    public void MakeSelection(int index)
+    public void OnPrediction(Prediction prediction)
     {
-        InputValue = index > 0;
+        InputValue = prediction.Index > 0;
     }
 
 
