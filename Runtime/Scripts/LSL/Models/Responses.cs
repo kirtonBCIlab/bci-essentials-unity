446936b12da5036a98a304d8170b57077ae09ce3
Twig <git@twig.gay>
2026-02-25 11:17:13 -0700

    Fixed prediction label indexing - restored index adjustment from 1-indexed prediction labels - extracted prediction classes to separate script

--- Runtime/Scripts/LSL/Models/Responses.cs
+++ Runtime/Scripts/LSL/Models/Responses.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System;
 
 namespace BCIEssentials.LSLFramework
 {
@@ -134,76 +133,5 @@ namespace BCIEssentials.LSLFramework
 
         public new static Response Parse(string body)
         => new MarkerReceipt {MarkerBody = body};
-    }
-
-
-    /// <summary>
-    /// Prediction/Selection response from bci-essentials python back end.
-    /// <br/><i>(0-indexed)</i>
-    /// </summary>
-    public class Prediction : SingleChannelResponse
-    {
-        /// <summary>
-        /// Index of object or class to select <i>(0-indexed)</i>
-        /// </summary>
-        public int Index { get; protected set; }
-        /// <summary>
-        /// Confidence ratio for each possible class or stimulus item
-        /// </summary>
-        public float[] Probabilities { get; protected set; }
-
-
-        public static Prediction Parse(string[][] predictionSegments)
-        => predictionSegments switch
-        {
-            { Length: 1 } => ParseValues(predictionSegments[0]),
-            _ => CompositePrediction.Parse(predictionSegments)
-        };
-
-        public static Prediction ParseValues(string[] valueStrings)
-        {
-            try
-            {
-                return new Prediction()
-                {
-                    Index = int.Parse(valueStrings[0]),
-                    Probabilities = valueStrings[1].Split(" ")
-                        .Select(s => float.Parse(s)).ToArray()
-                };
-            }
-            catch (Exception ex)
-            {
-                throw new FormatException
-                (
-                    $"Body segments of {typeof(Prediction).Name}"
-                    + $"were in unexpected format: {valueStrings}"
-                    , ex
-                );
-            }
-        }
-    }
-
-    /// <summary>
-    /// Prediction/Selection response containing multiple results,
-    /// <br>it is recommended to use the most recent result
-    /// </summary>
-    public class CompositePrediction : Prediction
-    {
-        public Prediction[] Parts { get; protected set; }
-
-        public new static CompositePrediction Parse(string[][] predictionSegments)
-        {
-            Prediction[] parts = predictionSegments.Select(
-                valueStrings => ParseValues(valueStrings)
-            ).ToArray();
-            Prediction latest = parts[^1];
-
-            return new()
-            {
-                Index = latest.Index,
-                Probabilities = latest.Probabilities,
-                Parts = parts
-            };
-        }
     }
 }
